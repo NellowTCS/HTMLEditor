c0232d8f3617ecf22ada85f79eef363be963c9f3
NellowTCS <neeljaiswal23@gmail.com>
2025-05-28 01:07:58 -0600

    scope fix

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,18 @@
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open("v1").then((cache) => {
-            return cache.addAll(["/HTMLEditor", "/HTMLEditor/index.html"]);
-        })
-    );
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      return cache.addAll([
+        "/HTMLEditor/",
+        "/HTMLEditor/index.html"
+      ]);
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
